65f1484df43601030c38f2b705fb2a2aa8652199
richardcrng@gmail.com <richardcrng@gmail.com>
2021-10-19 17:13:23 +0100

    Add new game selectors

--- client/src/modules/game/lobby/setup/GameLobbySetupEdit.tsx
+++ client/src/modules/game/lobby/setup/GameLobbySetupEdit.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+import { Button } from "semantic-ui-react";
+import { selectRolesInSetup } from "../../../../selectors/game";
 import { Game } from "../../../../types/game.types";
 import { ALL_ROLES, RoleKey } from "../../../../types/role.types";
-import RoleCard from "../../../role/card/RoleCard";
+import RoleCard, { getColors } from "../../../role/card/RoleCard";
 import RoleDropdown from "../../../role/dropdown/RoleDropdown";
 
 interface Props {
@@ -8,11 +11,31 @@ interface Props {
 }
 
 function GameLobbySetupEdit({ game }: Props) {
+  
+  const [selectedRole, setSelectedRole] = useState<RoleKey>()
+  const rolesInSetup = selectRolesInSetup(game);
 
   return (
     <>
       <p>Edit setup here!</p>
-      <RoleDropdown />
+      <ul>
+        {rolesInSetup.map(([role, count]) => (
+          <li key={role.key}>
+            <span
+              style={{ color: getColors(role.color).primary }}
+            >
+              {role.roleName}
+            </span> x {count}
+          </li>
+        ))}
+      </ul>
+      <RoleDropdown
+        selectedRole={selectedRole}
+        onRoleSelect={(role) => setSelectedRole(role)}
+      />
+      <Button fluid primary disabled={!selectedRole}>
+        Add role
+      </Button>
     </>
   );
 }
--- client/src/selectors/game.ts
+++ client/src/selectors/game.ts
@@ -1,10 +1,12 @@
 import { last } from 'lodash';
 import { createSelector } from 'reselect';
 import { Card, CardType, Game, GameBase, Round } from "../types/game.types";
+import { ALL_ROLES, PlayerRole, RoleKey } from '../types/role.types';
 
 
 
 export const selectGamePlayers = (game: Game) => game.players;
+export const selectGameRolesInPlayCount = (game: Game): Record<RoleKey, number> => game.roles.inPlay
 
 export const selectGamePlayersList = createSelector(
   selectGamePlayers,
@@ -26,4 +28,19 @@ export const selectGameLobbyReadiness = createSelector(
   count => count >= 6
     ? { isReady: true }
     : { isReady: false, reason: "Minimum 6 players needed" } 
+)
+
+export const selectGameRoleEntries = createSelector(
+  selectGameRolesInPlayCount,
+  (rolesCount) => Object.entries(rolesCount) as [RoleKey, number][]
+)
+
+export const selectGameRoleEntriesInGame = createSelector(
+  selectGameRoleEntries,
+  (entries) => entries.filter(([_, count]) => count > 0)
+);
+
+export const selectRolesInSetup = createSelector(
+  selectGameRoleEntriesInGame,
+  (entries) => entries.map(([roleKey, roleCount]) => [ALL_ROLES[roleKey], roleCount]) as [PlayerRole, number][]
 )
