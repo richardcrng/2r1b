6fb8d1d358aaff8249b5ed69946333d60f7b6310
richardcrng@gmail.com <richardcrng@gmail.com>
2021-10-19 16:24:05 +0100

    Use role card viewer in reference view

--- client/src/routes/ReferenceRoute.tsx
+++ client/src/routes/ReferenceRoute.tsx
@@ -1,5 +1,4 @@
 import { Redirect, Route, Switch, useHistory, useRouteMatch } from "react-router-dom";
-import RoleCard, { getColors } from "../modules/role/card/RoleCard";
 import RoleCardViewer from "../modules/role/card/viewer/RoleCardViewer";
 import { ALL_ROLES } from "../types/role.types";
 
@@ -8,29 +7,48 @@ const roleKeys = Object.keys(ALL_ROLES) as (keyof typeof ALL_ROLES)[]
 
 function ReferenceRoute() {
   const { path, url } = useRouteMatch();
+  const history = useHistory()
 
   return (
     <Switch>
       <Route exact path={`${path}/roles`}>
-        <div>
-          <RoleCardViewer />
-        </div>
+        <RoleCardViewer
+          onRoleSelect={(newRole) =>
+            history.push(`${url}/roles/${newRole.toLowerCase()}`)
+          }
+        />
       </Route>
       {roleKeys.map((roleKey) => (
-        <Route key={roleKey} exact path={`${path}/roles/${roleKey.toLowerCase()}`}>
-          <h1>Role Reference</h1>
-          <RoleCard role={ALL_ROLES[roleKey]} />
+        <Route
+          key={roleKey}
+          exact
+          path={`${path}/roles/${roleKey.toLowerCase()}`}
+        >
+          <RoleCardViewer
+            selectedRole={roleKey}
+            onRoleSelect={(newRole) =>
+              history.push(`${url}/roles/${newRole.toLowerCase()}`)
+            }
+          />
         </Route>
       ))}
       {roleKeys.map((roleKey) => (
         <Route
           key={roleKey}
           exact
-          path={`${path}/roles/${roleKey.replace(/(_RED|_BLUE|_GREY)/, "").toLowerCase()}`}
+          path={`${path}/roles/${roleKey
+            .replace(/(_RED|_BLUE|_GREY)/, "")
+            .toLowerCase()}`}
         >
           <Redirect to={`${path}/roles/${roleKey.toLowerCase()}`} />
         </Route>
       ))}
+      <Route exact path={`${path}/roles/blue_team`}>
+        <Redirect to={`${path}/roles/team_blue`} />
+      </Route>
+      <Route exact path={`${path}/roles/red_team`}>
+        <Redirect to={`${path}/roles/team_red`} />
+      </Route>
     </Switch>
   );
 }
