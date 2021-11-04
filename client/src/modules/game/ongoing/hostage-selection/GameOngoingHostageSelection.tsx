610897bce408ecd1627aeba05d5905ce2cd7bcc4
Richard Ng <richardcrng@gmail.com>
2021-11-04 21:28:37 +0000

    Show button to finalise hostages

--- client/src/modules/game/ongoing/hostage-selection/GameOngoingHostageSelection.tsx
+++ client/src/modules/game/ongoing/hostage-selection/GameOngoingHostageSelection.tsx
@@ -6,7 +6,21 @@ import { Game, Player, RoomName, Round } from '../../../../types/game.types';
 import PlayerDropdown from '../../../player/dropdown/PlayerDropdown';
 
 const Container = styled.div`
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-rows: 1fr auto;
+  grid-template-areas:
+    "main"
+    "actions";
+  width: 100%;
+`;
 
+const Main = styled.div`
+  grid-area: main;
+`
+
+const Actions = styled.div`
+  grid-area: actions;
 `
 
 const HostageOl = styled.ol`
@@ -41,58 +55,67 @@ function GameOngoingHostageSelection({ game, leaderName, isLeader, onHostageSele
 
   return (
     <Container className="active-contents">
-      <h1>Hostage{hostageTotal === 1 ? "" : "s"} selection</h1>
-      <p>
-        This round, {hostageTotal} hostage{hostageTotal === 1 ? "" : "s"} must
-        be sent to the other room.
-      </p>
-      {isLeader ? (
-        <>
-          <p>
-            As Room Leader, you must choose the hostage
-            {hostageTotal === 1 ? "" : "s"}.
-          </p>
-          <PlayerDropdown
-            filter={(player) =>
-              round.playerAllocation[player.socketId] === roomName &&
-              !currentHostages.includes(player.socketId)
-            }
-            onPlayerSelect={setSelectedPlayerId}
-            players={players}
-          />
-          <Button
-            disabled={!selectedPlayerId || hostageTotal === currentHostages.length}
-            fluid
-            onClick={() => onHostageSelect(selectedPlayerId!, roomName)}
-            primary
-          >
-            Add hostage
-          </Button>
-        </>
-      ) : (
-        <p>Waiting for {leaderName} to select hostages</p>
-      )}
-      <hr />
-      {currentHostages.length === 0 ? (
-        <p>No hostage{hostageTotal === 1 ? "" : "s"} selected</p>
-      ) : (
-        <HostageOl>
-          {currentHostages.map((playerId) => (
-            <HostageLi key={playerId}>
-              <div>
-                <span>{players[playerId].name}</span>
-                {isLeader && (
-                  <button
-                    onClick={() => onHostageSelect(playerId, roomName, true)}
-                  >
-                    X
-                  </button>
-                )}
-              </div>
-            </HostageLi>
-          ))}
-        </HostageOl>
-      )}
+      <Main>
+        <h1>Hostage{hostageTotal === 1 ? "" : "s"} selection</h1>
+        <p>
+          This round, {hostageTotal} hostage{hostageTotal === 1 ? "" : "s"} must
+          be sent to the other room.
+        </p>
+        {isLeader ? (
+          <>
+            <p>
+              As Room Leader, you must choose the hostage
+              {hostageTotal === 1 ? "" : "s"}.
+            </p>
+            <PlayerDropdown
+              filter={(player) =>
+                round.playerAllocation[player.socketId] === roomName &&
+                !currentHostages.includes(player.socketId)
+              }
+              onPlayerSelect={setSelectedPlayerId}
+              players={players}
+            />
+            <Button
+              disabled={
+                !selectedPlayerId || hostageTotal === currentHostages.length
+              }
+              fluid
+              onClick={() => onHostageSelect(selectedPlayerId!, roomName)}
+              primary
+            >
+              Add hostage
+            </Button>
+          </>
+        ) : (
+          <p>Waiting for {leaderName} to select hostages</p>
+        )}
+        <hr />
+        {currentHostages.length === 0 ? (
+          <p>No hostage{hostageTotal === 1 ? "" : "s"} selected</p>
+        ) : (
+          <HostageOl>
+            {currentHostages.map((playerId) => (
+              <HostageLi key={playerId}>
+                <div>
+                  <span>{players[playerId].name}</span>
+                  {isLeader && (
+                    <button
+                      onClick={() => onHostageSelect(playerId, roomName, true)}
+                    >
+                      X
+                    </button>
+                  )}
+                </div>
+              </HostageLi>
+            ))}
+          </HostageOl>
+        )}
+      </Main>
+      <Actions>
+        {isLeader && (
+          <Button color='black' fluid>Submit hostages</Button>
+        )}
+      </Actions>
     </Container>
   );
 }
