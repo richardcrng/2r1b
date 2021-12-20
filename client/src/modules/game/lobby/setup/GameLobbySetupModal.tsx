7dab92c91bf2f68dab60e007504a3fd71ea134ce
Richard Ng <richardcrng@gmail.com>
2021-12-20 11:23:29 +0000

    List errors and warnings in setup modal

--- client/src/modules/game/lobby/setup/GameLobbySetupModal.tsx
+++ client/src/modules/game/lobby/setup/GameLobbySetupModal.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { Modal } from "semantic-ui-react";
+import styled from "styled-components";
 import {
   selectGamePlayerCount,
   selectGameRolesInSetupCount,
+  selectGameSetupErrorsAndWarnings,
   selectNumberOfRolesInSetup,
   selectRolesInSetupAlphabetised,
 } from "../../../../selectors/game-selectors";
@@ -20,6 +22,25 @@ interface Props {
   onRoleIncrement(roleKey: RoleKey, increment: number): void;
 }
 
+const ErrorAndWarningList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+
+  li {
+    padding-left: 1rem;
+    text-indent: -0.7rem;
+  }
+
+  li.error::before {
+    content: "🚨 ";
+  }
+
+  li.warning::before {
+    content: "⚠️ ";
+  }
+`;
+
 function GameLobbySetupModal({
   game,
   isEditable,
@@ -33,14 +54,38 @@ function GameLobbySetupModal({
   const nRoles = selectNumberOfRolesInSetup(game);
   const rolesCount = selectGameRolesInSetupCount(game);
   const rolesInSetup = selectRolesInSetupAlphabetised(game);
+  const { errors, warnings } = selectGameSetupErrorsAndWarnings(game);
 
   return (
     <Modal {...{ onClose, onOpen }} closeIcon open={isOpen}>
-      <Modal.Header>Role setup</Modal.Header>
+      <Modal.Header>
+        Setup: {nPlayers} players, {nRoles} roles
+      </Modal.Header>
       <Modal.Content>
-        <p>
-          {nPlayers} players, {nRoles} roles
-        </p>
+        {(errors.length || warnings.length) && (
+          <details>
+            <summary
+              style={{
+                color: errors.length ? "red" : "orange",
+                fontWeight: "bold",
+              }}
+            >
+              {errorAndWarningsTitle(errors.length, warnings.length)}
+            </summary>
+            <ErrorAndWarningList>
+              {errors.map((error) => (
+                <li className="error" key={error.message}>
+                  {error.message}
+                </li>
+              ))}
+              {warnings.map((warning) => (
+                <li className="warning" key={warning.message}>
+                  {warning.message}
+                </li>
+              ))}
+            </ErrorAndWarningList>
+          </details>
+        )}
         <RoleSetup {...{ isEditable, rolesInSetup, onRoleIncrement }} />
         {isEditable && (
           <RoleAdder
@@ -52,5 +97,23 @@ function GameLobbySetupModal({
     </Modal>
   );
 }
+
+const errorAndWarningsTitle = (nErrors: number, nWarnings: number): string => {
+  const messages: string[] = [];
+
+  if (nErrors > 1) {
+    messages.push(`${nErrors} errors`);
+  } else if (nErrors === 1) {
+    messages.push(`${nErrors} error`);
+  }
+
+  if (nWarnings > 1) {
+    messages.push(`${nWarnings} warnings`);
+  } else if (nWarnings === 1) {
+    messages.push(`${nWarnings} warning`);
+  }
+
+  return messages.join(", ");
+};
 
 export default GameLobbySetupModal;
