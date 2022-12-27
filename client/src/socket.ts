be9266261f733f44cfa8d3178d60b8c40aca1496
Richard Ng <richardcrng@gmail.com>
2022-12-27 21:12:12 +0000

    Change socket URL in prod

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -4,7 +4,7 @@ import { ClientSocket } from "./types/event.types";
 
 export const socketUrl =
   process.env.NODE_ENV === "production"
-    ? "https://two-rooms-and-a-boom-server.herokuapp.com"
+    ? "https://2r1b.server.rcr.dev"
     : "http://localhost:4000";
 
 export const socket: ClientSocket = io(socketUrl);
